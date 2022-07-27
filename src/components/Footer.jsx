12e768c3daf2de4d046356ab009e738110c6fb23
Peter Okorafor <peterdtitan@gmail.com>
2022-07-27 17:40:18 +0300

    Update Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,25 +10,37 @@ import Facebook from '../img/facebook.svg';
 const Footer = () => {
   return (
     <div id='get-social' 
-    className=' bg-secondary '>
+    className='bg-secondary '>
         <div className='flex justify-center items-center py-6 md:py-8'>
             <h1 className='text-[1.5rem] lg:text-[2.5rem] text-textColor font-bold tracking-wide underline underline-offset-8'
             >Get Social
             </h1>
         </div>
 
-        <div className='flex flex-col md:flex-row px-4 md:px-16 py-6 items-center gap-8 justify-between'>
-            <div className="hidden md:flex flex-col items-start gap-2 cursor-pointer">
-                <img src={Logo} className="w-40 object-cover" alt="logo" />
-                <p>Afrocentric Kulture LLC.</p>
+        <div className='flex flex-col md:flex-row px-4 md:px-16 py-6 items-center md:items-start gap-8 justify-between'>
+            <div className="hidden md:flex md:flex-col items-start gap-2 cursor-pointer">
+                <img src={Logo} className="w-48 object-cover" alt="logo" />
+                <div className='text-textColor text-sm py-6'>
+                    <p>Afrocentric Kulture LLC.</p>
+                    <p>Licensed, registered in the US</p>
+                </div>
             </div>
 
-            <div className='flex items-center gap-4 sm:justify-center cursor-pointer'>
-                <a href='https://www.linkedin.com/company/afrocentric-kulture' target='_blank'><img src={Twitter} className='h-10 w-10' /></a>
-                <a href='https://www.linkedin.com/company/afrocentric-kulture' target='_blank'><img src={Instagram} className='h-10 w-10' /></a>
-                <a href='https://www.linkedin.com/company/afrocentric-kulture' target='_blank'><img src={Linkedin} className='h-10 w-10' /></a>
-                <a href='https://www.linkedin.com/company/afrocentric-kulture' target='_blank'><img src={Facebook} className='h-10 w-10' /></a>
+            <div className='flex flex-col'>
+                <h2>CONTACT US</h2>
+
             </div>
+
+            <div className='flex flex-col'>
+                <h2>FOLLOW US</h2>
+                <div className='flex items-center gap-4 sm:justify-center cursor-pointer'>
+                    <a href='https://twitter.com/AfrocentricKul1' target='_blank'><img src={Twitter} className='h-10 w-10' /></a>
+                    <a href='https://www.instagram.com/afrocentrickulture/' target='_blank'><img src={Instagram} className='h-10 w-10' /></a>
+                    <a href='https://www.linkedin.com/company/afrocentric-kulture' target='_blank'><img src={Linkedin} className='h-10 w-10' /></a>
+                    <a href='https://www.facebook.com/AFKulture' target='_blank'><img src={Facebook} className='h-10 w-10' /></a>
+                </div>
+            </div>
+
         </div>
     </div>
   )
