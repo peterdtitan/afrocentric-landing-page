ea5c7d8bdb243c761f57f9fed57ea52b335a05ec
Peter Okorafor <peterdtitan@gmail.com>
2022-07-28 16:06:07 +0300

    Finished Footer

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -2,8 +2,7 @@ import React from 'react';
 
 const Services = () => {
   return (
-    <div className="px-4 text-[2.5rem] lg:text-[4.5rem] font-bold tracking-wide text-headingColor">
-      <p>I Love you Zam Zam</p>
+    <div>
     </div>
   )
 }
